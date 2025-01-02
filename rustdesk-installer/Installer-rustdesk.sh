4faf901dbd9c4664978e797e89de0d24abbe953a
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2025-01-02 10:24:39 +0100

    fix: update code to be more bullet-proof

--- rustdesk-installer/Installer-rustdesk.sh
+++ rustdesk-installer/Installer-rustdesk.sh
@@ -1,41 +1,39 @@
-#/bin/bash
+#!/bin/bash
 
 version=1.3.6
 
 # Vérifier si la connexion internet est active
-ping6 -c 3 www.example.com
-if [ $? -eq 0 ]; then
+if ! ping -c 3 www.example.com; then
+	echo -e "Pas de connexion internet, verifier votre connexion et ré-essayez"
+	exit 1
+fi
 
-cd ~/Téléchargements
+cd "${HOME}/Téléchargements" || exit 1
 
 echo -e "téléchargement du paquet rustdesk\n"
-wget -c https://github.com/rustdesk/rustdesk/releases/download/$version/rustdesk-$version-x86_64.deb
+wget -c "https://github.com/rustdesk/rustdesk/releases/download/${version}/rustdesk-${version}-x86_64.deb"
 
 sleep 2
 
-echo -e "Le paquet gstreamer1.0-pipewire est-il installé ? Si il n'est pas installé on l'installe\n"
+cat <<EOF
+Le paquet gstreamer1.0-pipewire est-il installé ? Si il n'est pas installé on l'installe.
+De plus, nous ajoutons la source et installons la version fournie par le paquet communautaire pipewire-debian.
+EOF
 sleep 2
-echo -e "De plus nous ajoutons la source et installons la version fournie par le paquet communautaire pipewire-debian\n"
 
 pkg=gstreamer1.0-pipewire
 
-status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
-if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
-	sudo add-apt-repository ppa:pipewire-debian/pipewire-upstream -y 2>&1
+if ! status="$(dpkg-query -W --showformat='${db:Status-Status}' "${pkg}" 2>&1)" || [[ ! "${status}" = installed ]]; then
+	echo sudo add-apt-repository ppa:pipewire-debian/pipewire-upstream -y 2>&1
 	sudo apt update -y
-	sudo apt install $pkg -y
+	sudo apt install "${pkg}" -y
+	sleep 3
 fi
 
-sleep 3
 echo -e "installation du paquet rustdesk précédemment téléchargé\n"
-sudo dpkg -i ~/Téléchargements/rustdesk-$version-x86_64.deb 
+sudo dpkg -i "${HOME}/Téléchargements/rustdesk-${version}-x86_64.deb"
 
 sleep 2
 
 echo -e "Si il manque une dépendance, en forcer l'installation\n"
 sudo apt-get -f install -y
-
-else
-	echo -e "Pas de connexion internet, verifier votre connexion et ré-essayez"
-fi
-
