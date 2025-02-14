7d416161cf75ecbef895651141653843d168208e
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2025-02-14 19:32:31 +0100

    fix: change logic in scripts

--- bashtrans/bashtrans.sh
+++ bashtrans/bashtrans.sh
@@ -5,28 +5,18 @@
 # Licence GPL
 # Generate pot, and *-language.po files, to translate bash scripts
 # Aug Sun 08 2010
-# 
-Encoding=UTF-8
 #
-usage ()
-{
-echo "No File"
-exit 1 #exit script
-}
-#
-if [ $# -eq 0 ];
-then
-usage
+
+if [ $# -eq 0 ]; then
+    echo "No File"
+    exit 1 #exit script
 fi
-#
+
 SCRIPT="$1"
-#
+
 SAVE=$(zenity --file-selection --title "Bash-Script-Translator-Generator" --save)
-if [ "$SAVE" = "" ];then
-exit
-else
-LOCALE=$(cat /usr/share/i18n/locales/i18n |grep LC_CTYPE |tail -1 |cut -c 10-14)
-bash --dump-po-strings "$SCRIPT" | xgettext -L PO -o $SAVE.pot -
-xterm -e "msginit -l $LOCALE -i $SAVE.pot -o $SAVE-$LOCALE.po"
+if [ -n "$SAVE" ];then
+    LOCALE=$(grep LC_CTYPE /usr/share/i18n/locales/i18n | tail -1 | cut -c 10-14)
+    bash --dump-po-strings "$SCRIPT" | xgettext -L PO -o "$SAVE.pot" -
+    xterm -e "msginit -l $LOCALE -i $SAVE.pot -o $SAVE-$LOCALE.po"
 fi
-exit
