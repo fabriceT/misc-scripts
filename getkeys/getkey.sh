7295ec5e0d5014f43a6e5645cd048b00dc2c75b3
Joyce MARKOLL <contact@orditux.org>
2026-07-13 01:40:41 +0200

    Script changed to use the gpg signing key method
    Signed-off-by: Joyce MARKOLL <contact@orditux.org>

--- getkeys/getkey.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export TEXTDOMAIN=getkey.sh
-export TEXTDOMAINDIR="/usr/share/locale"
-
-echo $"Please provide the key signature you want to add?"
-read -r KEY
-
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com "$KEY"
